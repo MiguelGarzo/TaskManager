73537c684ca3786b29f89a812e499e2d649dddbe
MiguelGarzo <miguel.garzo53@gmail.com>
2026-02-27 12:37:02 +0100

    Repositories updating for separate by company id

--- src/main/java/com/taskmanager/TaskManager/commentary/repository/CommentaryRepository.java
+++ src/main/java/com/taskmanager/TaskManager/commentary/repository/CommentaryRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CommentaryRepository extends JpaRepository<Commentary, Long> {
-    Optional<Commentary> findById(Long ComentId);
+    Optional<Commentary> findByIdAndCompanyId(Long commentId, Long companyId);
 }
--- src/main/java/com/taskmanager/TaskManager/subtask/repository/SubtaskRepository.java
+++ src/main/java/com/taskmanager/TaskManager/subtask/repository/SubtaskRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface SubtaskRepository extends JpaRepository<Subtask, Long> {
-    Optional<Subtask> findById(Long subtaskId);
+    Optional<Subtask> findByIdAndCompanyId(Long subtaskId, Long companyId);
 }
